15b53d249be17bb2b5249d360b91a6a0b6dbf5fc
Jeremy Lopez <lopez703@umn.edu>
2018-08-10 14:56:33 -0500

    -adding a "clear all" button that will delete all directories in listbox -code cleanup -adding comments to code

--- LS_GUI.py
+++ LS_GUI.py
@@ -1,6 +1,7 @@
-####### Retrieve a list of directories with wxPython-Phoenix   - tested on python3.5
 ### installation instruction for wxPython-Phoenix  : https://wiki.wxpython.org/How%20to%20install%20wxPython#Installing_wxPython-Phoenix_using_pip
-### modified from : https://wxpython.org/Phoenix/docs/html/wx.lib.agw.multidirdialog.html
+### modified from : https://stackoverflow.com/questions/28417602/ask-multiple-directories-dialog-in-tkinter
+###					https://wxpython.org/pages/overview/#hello-world
+
 import os
 import wx
 import wx.lib.agw.multidirdialog as MDD
@@ -8,67 +9,83 @@ import wx.lib.agw.multidirdialog as MDD
 class MultiSelect(wx.Frame):
 	# Our normal wxApp-derived class, as usual
 	app = wx.App(0)
+
+	#menu where directory selection(s) will be made
 	dlg = MDD.MultiDirDialog(None, title="Custom MultiDirDialog", defaultPath=os.getcwd(),  # defaultPath="C:/Users/users/Desktop/",
     	             	    agwStyle=MDD.DD_MULTIPLE|MDD.DD_DIR_MUST_EXIST)
-	#setup listbox as global var
-	init_list = []
+	
+	#setup listbox list as global var
+	init_list = []	
 	cont = None
 
 	def __init__(self, *args, **kargs):
 		super(MultiSelect, self).__init__(*args, **kargs)
+
+		#panel setup for button and listbox use
 		panel = wx.Panel(self, size = (400,300))
-		exitbutton = wx.Button(panel, label="Exit", pos = (150,150), size = (100,60))
+
+		#app contains 4 buttons for interactive use
+		exitbutton = wx.Button(panel, label="Exit", pos = (150,140), size = (100,60))
+		clearbutton = wx.Button(panel, label="Clear All", pos = (150,180), size = (100,60))
 		addbutton = wx.Button(panel, label = "+", pos = (50,50), size = (40,40))
 		delbutton = wx.Button(panel, label = "-", pos = (50,110), size = (40,40))
 
-		#bind button with functions
+		#event handling - button binded with functions
+		self.Bind(wx.EVT_BUTTON, self.on_exit_button, exitbutton)
+		self.Bind(wx.EVT_BUTTON, self.on_clear_button, clearbutton)
 		self.Bind(wx.EVT_BUTTON, self.on_add_button, addbutton)
 		self.Bind(wx.EVT_BUTTON, self.on_del_button, delbutton)
-		self.Bind(wx.EVT_BUTTON, self.on_exit_button, exitbutton)
+
+		#event handling - close app with "x" button located in corner
 		self.Bind(wx.EVT_CLOSE, self.closewindow)
 
 		#setup listbox
 		self.cont = wx.ListBox(panel, -1, (100,50), (200,100), self.init_list, wx.LB_EXTENDED)
 
-
+	#opens a new menu and closes when selection(s) is made
+	#adds currently selected directory/directories to listbox
 	def on_add_button(self, event):
-		#self.reset_listbox()
 		self.main()
 
+	#deletes currently selected directory/directories from listbox
 	def on_del_button(self,event):
 		deleted_items = self.cont.GetSelections()
 		deleted_items.reverse()
 		for file in deleted_items:
 			self.cont.Delete(file)
 	
+	#closes window, does NOT terminate app
 	def on_exit_button(self, event):
 		self.dlg.Destroy()
 		self.Close(True)
-		#BUG: not closing properly
 
-	def closewindow(self, event):
-		self.Destroy()
-
-	def reset_listbox(self):
+	#deletes all current directories in listbox
+	def on_clear_button(self, event):
 		while (self.cont.GetCount() != 0):
 			self.cont.Delete(0)
 
+	#closes app
+	def closewindow(self, event):
+		self.Destroy()
+
+	#obtains path for selected directory to be added to the listbox
 	def main(self):
 		if self.dlg.ShowModal() != wx.ID_OK:
 			print("You Cancelled The Dialog!")
 			self.dlg.Destroy()
-		paths = self.dlg.GetPaths()
-		#Print directories' path and files 
-		dir_list = []
+
+		#obtains currently selected directory path
+		paths = self.dlg.GetPaths() 
 		for path in enumerate(paths):
-			#print(path[1])
-			directory= path[1].replace('Macintosh HD','')
-			#windows os: directory= path[1].replace('OS (C:)','C:')
-			print(directory)
-			for file in os.listdir(directory):
-				dir_list.append(file)
-				print(file)
-		self.cont.InsertItems(dir_list, 0)
+
+			#adds string of currently selected directory to listbox
+			#for windows os: dir_path= path[1].replace('OS (C:)','C:')
+			dir_path = path[1].replace('Macintosh HD','')
+			directory = [os.path.basename(dir_path)]
+			self.cont.InsertItems(directory, 0)
+
+			#any additional functionalty for selected directory 
+			# should be added here
 
 if __name__ == '__main__':
 	app = wx.App()
