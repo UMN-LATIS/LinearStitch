fd989a37bd42678e8372c700e927da950fe0f059
Colin McFadden <colin@hedge.video>
2025-04-01 11:29:45 -0500

    make window updating threadsafe

--- LS_GUI.py
+++ LS_GUI.py
@@ -662,13 +662,13 @@ class Processor:
 		return avg_color
 
 class CustomOutputWindow(wx.Frame):
-    def __init__(self):
-        wx.Frame.__init__(self, None, -1, "LinearStitch Output", 
+	def __init__(self):
+		wx.Frame.__init__(self, None, -1, "LinearStitch Output", 
                           pos=(50, 50), size=(600, 300))
-        self.text = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE|wx.TE_READONLY)
+		self.text = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE|wx.TE_READONLY)
         
-    def write(self, text):
-        self.text.AppendText(text)
+	def write(self, text):
+		wx.CallAfter(self.text.AppendText, text)
 class MyApp(wx.App):
 	def OnInit(self):
 		self.outputWindow = CustomOutputWindow()
